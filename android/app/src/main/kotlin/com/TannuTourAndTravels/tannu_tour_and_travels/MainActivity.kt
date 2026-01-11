861df4aecf1b0de0ffb7aa916bcfcb1271844ce8
Manish <manish@dbsindia.in>
2026-01-11 22:55:27 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/TannuTourAndTravels/tannu_tour_and_travels/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.TannuTourAndTravels.tannu_tour_and_travels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:tannu_tour_and_travels/my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tannu Tour And Travels',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
